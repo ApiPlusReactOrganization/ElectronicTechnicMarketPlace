517060f618d1caf721bd78707b6ea6a3f21d9039
Marian-Zharchynskyi <145098603+Marian-Zharchynskyi@users.noreply.github.com>
2024-11-24 00:41:00 +0200

    Loader was added

---
+++ electronic-marketplace/src/hooks/useLoader.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const useLoader = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  return {
+    isLoading,
+    setIsLoading,
+  };
+};
+
+export default useLoader;
--- electronic-marketplace/src/pages/manufacturers/components/ManufacturerTableContainer.jsx
+++ electronic-marketplace/src/pages/manufacturers/components/ManufacturerTableContainer.jsx
@@ -2,13 +2,25 @@ import React, { useState, useEffect, useCallback } from 'react'
 import useActions from '../../../hooks/useActions'
 import ManufacturersTable from './ManufacturersTable'
 import AddManufacturerModal from './manufacturersModals/AddManufacturerModal'
+import Loader from '../../../components/Loader'
+import useLoader from '../../../hooks/useLoader'
 
 const ManufacturerTableContainer = () => {
   const { getManufacturers } = useActions()
+
   const [showAddModal, setShowAddModal] = useState(false)
 
+  const { isLoading, setIsLoading } = useLoader()
+
   useEffect(() => {
+    setIsLoading(true)
     getManufacturers()
+      .then(() => {
+        setIsLoading(false)
+      })
+      .catch(() => {
+        setIsLoading(false)
+      })
   }, [])
 
   const openAddModal = useCallback(() => setShowAddModal(true), [])
@@ -16,16 +28,18 @@ const ManufacturerTableContainer = () => {
 
   return (
     <>
-      <button className="btn btn-primary mb-3" onClick={openAddModal}>
-        Add Manufacturer
-      </button>
+      <Loader isLoading={isLoading}>
+        <button className="btn btn-primary mb-3" onClick={openAddModal}>
+          Add Manufacturer
+        </button>
 
-      <ManufacturersTable />
+        <ManufacturersTable />
 
-      <AddManufacturerModal
-        showModal={showAddModal}
-        closeModal={closeAddModal}
-      />
+        <AddManufacturerModal
+          showModal={showAddModal}
+          closeModal={closeAddModal}
+        />
+      </Loader>
     </>
   )
 }
