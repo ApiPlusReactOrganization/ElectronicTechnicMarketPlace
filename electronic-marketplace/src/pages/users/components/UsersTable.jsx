95f5527760fae3339fdf54ef527045e780859bac
Marian-Zharchynskyi <145098603+Marian-Zharchynskyi@users.noreply.github.com>
2024-11-27 00:18:53 +0200

    Optimization for UsersTable V1 and for MyPage

--- electronic-marketplace/src/pages/users/components/UsersTable.jsx
+++ electronic-marketplace/src/pages/users/components/UsersTable.jsx
@@ -1,35 +1,34 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import UsersTableRow from "./UsersTableRow";
+import React from 'react'
+import { useSelector } from 'react-redux'
+import UsersTableRow from './UsersTableRow'
+import { useRenderCount } from '../../../hooks/useRenderCount'
 
-const UsersTable = ({ onRoleChange, onDelete }) => {
-  const { userList, roleList } = useSelector((state) => state.user);
-
+const UsersTable = () => {
+  const { userList, roleList } = useSelector((state) => state.user)
+  const renderCount = useRenderCount()
   return (
-    <table className="table table-striped table-bordered table-hover">
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Roles</th>
-          <th>Image</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {userList.map((user) => (
-          <UsersTableRow
-            key={user.id}
-            user={user}
-            roleList={roleList}
-            onRoleChange={onRoleChange}
-            onDelete={onDelete}
-          />
-        ))}
-      </tbody>
-    </table>
-  );
-};
+    <>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Roles</th>
+            <th>Avatar</th>
+            <th>Actions</th>
+            <th>Render Count</th>
+          </tr>
+        </thead>
+        <tbody>
+          {userList.map((user) => (
+            <UsersTableRow key={user.id} user={user} roleList={roleList} />
+          ))}
+        </tbody>
+      </table>
+      <h5>ManufacturersTable render count: {renderCount}</h5>
+    </>
+  )
+}
 
 export default React.memo(UsersTable);
--- electronic-marketplace/src/pages/users/components/UsersTableContainer.jsx
+++ electronic-marketplace/src/pages/users/components/UsersTableContainer.jsx
@@ -1,66 +1,25 @@
 import React, { useEffect } from "react";
 import useActions from "../../../hooks/useActions";
 import useLoader from "../../../hooks/useLoader";
-import UsersTable from "./UsersTable";
 import Loader from "../../../components/Loader";
 import { toast } from "react-toastify";
+import UsersTable from "./UsersTable";
 
 const UsersTableContainer = () => {
-  const { getUsers, getRoles, changeRoles, deleteUser } = useActions();
+  const { getUsers, getRoles } = useActions();
   const { isLoading, setIsLoading } = useLoader();
 
   useEffect(() => {
     setIsLoading(true);
     getUsers()
       .then(() => getRoles())
-      .then(() => {
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        toast.error("Failed to load data.");
-        setIsLoading(false);
-      });
+      .catch(() => toast.error("Failed to load data."))
+      .finally(() => setIsLoading(false));
   }, []);
-
-  const handleRoleChange = async (userId, newRoles) => {
-    const roles = newRoles.map((role) => ({ name: role.name }));
-    setIsLoading(true);
-    try {
-      const result = await changeRoles(userId, roles);
-
-      if (result.success) {
-        await getUsers();
-        await getRoles();
-      } else {
-        toast.error(result.message);
-      }
-    } catch (error) {
-      toast.error("Failed to change roles.");
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  const handleDelete = async (userId) => {
-    setIsLoading(true);
-    try {
-      const result = await deleteUser(userId);
-
-      if (result.success) {
-        await getUsers();
-      } else {
-        toast.error(result.message);
-      }
-    } catch (error) {
-      toast.error("Failed to delete user.");
-    } finally {
-      setIsLoading(false);
-    }
-  };
 
   return (
     <Loader isLoading={isLoading}>
-      <UsersTable onRoleChange={handleRoleChange} onDelete={handleDelete} />
+      <UsersTable />
     </Loader>
   );
 };
