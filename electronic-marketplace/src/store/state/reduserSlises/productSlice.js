61efcb05893f80775e7181e1f696d974ac788c12
mishalolka299 <mykhailo.lukianchuk@oa.edu.ua>
2024-12-13 20:26:19 +0200

    delete comments

--- electronic-marketplace/src/store/state/reduserSlises/productSlice.js
+++ electronic-marketplace/src/store/state/reduserSlises/productSlice.js
@@ -14,10 +14,7 @@ export const productSlice = createSlice({
     },
 
     getFilterProducts: (state, action) => {
-      // const isEqual = JSON.stringify(state.productList) === JSON.stringify(action.payload);
-      // if (!isEqual) {
         state.productList = action.payload;
-      // }
     },
 
     getProductsByCategory: (state, action) => {
